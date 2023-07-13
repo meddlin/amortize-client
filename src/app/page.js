a1d346461a8b1b17778af1eb15c4b018b688c736
meddlin <drushing.dev@gmail.com>
2023-07-13 16:58:31 -0500

    New table component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-
+import Table from './table';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
-        Table goes here
+        
+        <Table />
+
       </h1>
     </main>
   )
