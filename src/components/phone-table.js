c923e8e588f9f33d977b79909f85c5f56b321b62
meddlin <drushing.dev@gmail.com>
2023-08-06 00:07:08 -0500

    table visible on xs screen, limited to basic info

--- src/components/phone-table.js
+++ src/components/phone-table.js
@@ -20,7 +20,7 @@ const PhoneTable = ({ amortization }) => {
             }
         }),
         columnHelper.accessor('totalMonthlyCustomerPayment', {
-            header: () => <h3>Total Mon. Pmt.</h3>,
+            header: () => <h3 className="max-w-[75px]">Total Mon. Pmt.</h3>,
             cell: info => {
                 const value = info.getValue();
                 return value ? `$${value.toFixed(2)}` : value;
@@ -34,19 +34,19 @@ const PhoneTable = ({ amortization }) => {
             },
         }),
         columnHelper.accessor('remaining', {
-            header: () => <h3>Rem. Principal</h3>,
+            header: () => <h3 className="max-w-[75px]">Rem. Principal</h3>,
             cell: info => {
                 const value = info.getValue();
                 return value ? `$${value.toFixed(2)}` : value;
             },
         }),
-        columnHelper.accessor('extraMonthlyPayment', {
-            header: () => <h3>Extra Payment</h3>,
-            cell: info => {
-                const value = info.getValue();
-                return value ? `$${value.toFixed(2)}` : value;
-            },
-        }),
+        // columnHelper.accessor('extraMonthlyPayment', {
+        //     header: () => <h3>Extra Payment</h3>,
+        //     cell: info => {
+        //         const value = info.getValue();
+        //         return value ? `$${value.toFixed(2)}` : value;
+        //     },
+        // }),
     ];
 
     const table = useReactTable({
@@ -62,59 +62,45 @@ const PhoneTable = ({ amortization }) => {
     })
 
     return (
-        <div className="px-4 sm:px-6 lg:px-8">
-            <div className="flex items-center">
-                <div className="sm:flex-auto">
-                    <h1 className="text-base font-semibold leading-6 text-gray-900">Amortization</h1>
-                    <p className="mt-2 text-sm text-gray-700">
-                        See the calculated amortization table below.
-                    </p>
-                </div>
-            </div>
-            <div className="mt-8 flow-root">
-                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                        <table className="min-w-full divide-y divide-gray-300">
-                            <thead>
-                                {table.getHeaderGroups().map((headerGroup) => (
-                                    <tr key={headerGroup.id}>
-                                        {headerGroup.headers.map((header) => (
-                                            <th key={header.id} className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                {
-                                                    header.isPlaceholder 
-                                                    ? null 
-                                                    : (
-                                                        <>
-                                                            <div>
-                                                                {flexRender(
-                                                                    header.column.columnDef.header, header.getContext() 
-                                                                )}
-                                                            </div>
-                                                        </>
-                                                    )
-                                                }
-                                            </th>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </thead>
-                            <tbody className="divide-y divide-gray-200">
-                                {table.getRowModel().rows.map((row) => (
-                                    <tr 
-                                        key={row.id} 
-                                        className="leading-4 text-sm hover:bg-slate-100 hover:cursor-pointer">
-                                        {row.getVisibleCells().map(cell => (
-                                            <td key={cell.id} className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                            </td>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+        <div className="flex justify-center">
+            <table className="divide-y divide-gray-300">
+                <thead>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th key={header.id} className="px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    {
+                                        header.isPlaceholder 
+                                        ? null 
+                                        : (
+                                            <>
+                                                <div>
+                                                    {flexRender(
+                                                        header.column.columnDef.header, header.getContext() 
+                                                    )}
+                                                </div>
+                                            </>
+                                        )
+                                    }
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody className="divide-y divide-gray-200">
+                    {table.getRowModel().rows.map((row) => (
+                        <tr 
+                            key={row.id} 
+                            className="leading-4 text-sm hover:bg-slate-100 hover:cursor-pointer">
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id} className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
